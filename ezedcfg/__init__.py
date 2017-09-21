2085cf9ff099b85e2005738cb2ec3a1dc7e8c3bb
Stephen Flynn <stephen@flynn>
2017-09-20 21:17:44 -0700

    Bump version: 0.1.4 → 0.1.5

--- ezedcfg/__init__.py
+++ ezedcfg/__init__.py
@@ -3,6 +3,6 @@
 """Top-level package for EZedCfg."""
 
 __author__ = """Stephen Flynn"""
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 
 from .ezedcfg import EZedCfg, UnrecognizedFormatError
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='ezedcfg',
-    version='0.1.4',
+    version='0.1.5',
     description="A basic configuration loading package",
     long_description=readme + '\n\n' + history,
     author="Stephen Flynn",
